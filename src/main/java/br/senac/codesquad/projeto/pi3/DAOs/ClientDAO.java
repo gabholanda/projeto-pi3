6f9db216ff93fbe780d9676bd3431b7dc7339454
marcelo.moraes <marcelo.moraes1598@gmail.com>
2019-10-17 12:18:30 -0300

    DAO e Servlet.

--- src/main/java/br/senac/codesquad/projeto/pi3/DAOs/ClientDAO.java
+++ src/main/java/br/senac/codesquad/projeto/pi3/DAOs/ClientDAO.java
@@ -6,7 +6,10 @@
 package br.senac.codesquad.projeto.pi3.DAOs;
 
 import br.senac.codesquad.projeto.pi3.models.Client;
-import br.senac.codesquad.projeto.pi3.models.Employee;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -15,24 +18,123 @@ import java.util.ArrayList;
  */
 public class ClientDAO {
 
-    //teste
+    private static Connection connection;
 
+    //teste
     public static ArrayList<Client> getClient() {
+
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public static boolean delete(int id) {
+        boolean returnn = false;
+
+        /*   try { 
+            
+            Class.forName(DRIVER); 
+            connection = DriverManager.getConnection(URL, LOGIN, SENHA); 
+            
+            PreparedStatement comand = connection.prepareStatement("DELETE FROM CLIENT WHERE ID_CLIENT=?"); 
+            
+            comand.setInt(1, id);
+            
+            int lineserror = comand.executeUpdate(); 
+            
+            if(lineserror > 0){ 
+                returnn = true; 
+            } else { 
+                returnn = false; 
+            }                        
+            
+        }catch (ClassNotFoundException | SQLException ex) { 
+            returnn = false; 
+        }finally { 
+            try { 
+                connection.close();
+            }catch (SQLException ex) { 
+                returnn = false; 
+            }
+        }
+        
+       return returnn; */
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public static boolean update(Client client) {
+
+        boolean returnn = false;
+
+        /*     try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, LOGIN, SENHA);
+
+            PreparedStatement comand = connection.prepareStatement("UPDATE CLIENT SET "
+                    + "NAME=?, ANDRESS=?"
+                    + "WHERE ID_CLIENT=?");
+
+            comand.setString(1, client.getName());
+            comand.setString(2, client.getAddress());
+
+            int lineserror = comand.executeUpdate();
+
+            if (lineserror > 0) {
+                returnn = true;
+            } else {
+                returnn = false;
+            }
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            returnn = false;
+
+        } finally { 
+            try { 
+                connection.close();
+            } catch (SQLException ex){ 
+                returnn = false; 
+            }
+        }
+        
+        return returnn; */
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public static boolean save(Client client) {
+
+        boolean returnn = false;
+
+        /*    try { 
+            Class.forName(DRIVER); 
+            connection = DriverManager.getConnection(URL, LOGIN, SENHA); 
+            
+            PreparedStatement comand = connection.preparedStatement("INSERT INTO CLIENT"
+                    + "(""""""""") " "
+                    + " VALUES(??????????)");  
+         
+                    comand.setString(1, client.getName());
+                    comand.setString(2, client.getCpf());
+                    comand.setString(3, client.getAddress()); 
+                    comand.setString(4, client.getMail()); 
+                    
+                    int lineserror = comand.executeUpdate(); 
+                    
+                    if (lineserror > 0) {
+                        returnn = true; 
+                    }else {
+                        returnn = false; 
+                    }
+                
+            }catch (ClassNotFoundException | SQLException ex){ 
+                returnn = false;                            
+        } finally { 
+            try{ 
+                connection.close(); 
+            }catch (SQLException ex){ 
+                returnn = false; 
+            }
+        }
+        
+        return returnn; */
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
-    
-} 
+}
--- src/main/java/br/senac/codesquad/projeto/pi3/DAOs/ProductDAO.java
+++ src/main/java/br/senac/codesquad/projeto/pi3/DAOs/ProductDAO.java
@@ -7,6 +7,9 @@ package br.senac.codesquad.projeto.pi3.DAOs;
 
 import br.senac.codesquad.projeto.pi3.models.Product;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -14,32 +17,128 @@ import java.util.ArrayList;
  * @author gabriel.hsantos21
  */
 public class ProductDAO {
-    
-    private static Connection conectionSystem; 
 
-   
+    private static Connection connection;
 
     public static ArrayList<Product> getProduct() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public static boolean delete(int id) {
+
+        boolean returnn = false;
+
+        /*  try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, LOGIN, SENHA);
+
+            PreparedStatement comand = connection.prepareStatement("DELETE FROM CLIENT WHERE ID_PRODUCT=?");
+                    
+            comand.setInt(1, id);
+
+            int lineserror = comand.executeUpdate();
+
+            if (lineserror > 0) {
+                returnn = true;
+            } else {
+                returnn = false;
+            }
+
+        } catch (SQLException ex) {
+            returnn = false;
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                returnn = false;
+            }
+        }
+
+        return returnn;
+
+    }*/
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public static boolean update(Product product) {
+
+        boolean returnn = false;
+
+        /*   try { 
+            
+            Class.forName(DRIVER); 
+            connection = DriverManager.getConnection(URL, LOGIN, SENHA); 
+            
+            PreparedStatement comand = connection.prepareStatement("UPDATE PRODUCT SET "
+                    + "NAME=?, AMOUNT=?, VALUES=?, VALUESSALE=?, DETAILS=?"); 
+            
+            comand.setString(1, product.getNameProduct());
+            comand.setInt(2, product.getAmount());
+            comand.setDouble(3, product.getValues());
+            comand.setDouble(4, product.getValeusSale());
+            comand.setString(5, product.getDetails());
+            
+            int lineserror = comand.executeUpdate(); 
+            
+             if (lineserror > 0) {
+                returnn = true;
+            } else {
+                returnn = false;
+            }
+
+        } catch (SQLException ex) {
+            returnn = false;
+
+        } finally { 
+            try { 
+                connection.close();
+            } catch (SQLException ex){ 
+                returnn = false; 
+            }
+        }
+        
+        return returnn; 
+        }  */
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public static boolean save(Product product) {
-        boolean returnn = false; 
-        throw new UnsupportedOperationException("Not supported yet.");
-       /* try { 
-            
-            Class.forName("driver do banco"); 
-            conectionSystem
+        boolean returnn = false;
+
+        /*  try {
+
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, LOGIN, SENHA);
+        
+            PreparedStatement comand = connection.prepareStatement("INSERT INTO PRODUCT "
+                    + "(NAME, AMOUNT, VALUES, VALUESSALE, DETAILS)"
+                    + "VALUES(?,?,?,?,?,)");
+
+            comand.setString(1, product.getNameProduct());
+            comand.setInt(2, product.getAmount());
+            comand.setDouble(3, product.getValues());
+            comand.setDouble(4, product.getValeusSale());
+            comand.setString(5, product.getDetails());
+
+            int lineserror = comand.executeUpdate();
+
+            if (lineserror > 0) {
+                returnn = true;
+            } else {
+                returnn = false;
+            }
+        } catch (SQLException ex) {
+            returnn = false;
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                returnn = false;
+            }
+
         }
+
+        return returnn; */
+        throw new UnsupportedOperationException("Not supported yet.");
     }
-   */ 
-}
 }
