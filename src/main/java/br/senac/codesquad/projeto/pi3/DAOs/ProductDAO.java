5012f08984afe10e67a209bfd32305001704e97f
giraweps.cyrillo@gmail.com <>
2019-10-18 19:42:07 -0300

    Alteração JSP, DAO, Servlet Controller de Product

--- src/main/java/br/senac/codesquad/projeto/pi3/DAOs/ProductDAO.java
+++ src/main/java/br/senac/codesquad/projeto/pi3/DAOs/ProductDAO.java
@@ -1,9 +1,12 @@
 package br.senac.codesquad.projeto.pi3.DAOs;
 
+import br.senac.codesquad.projeto.pi3.application.ConnectionManager;
 import br.senac.codesquad.projeto.pi3.models.Product;
+import static br.senac.codesquad.projeto.pi3.models.Product.getId;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -14,79 +17,90 @@ import java.util.ArrayList;
 public class ProductDAO {
 
     private static Connection connection;
+    private static PreparedStatement ps = null;
+    private static ResultSet rs = null;
+    private static boolean retorno = false; 
+    public static ArrayList<Product> getProduct() throws Exception {
+        ArrayList<Product> Product = new ArrayList<Product>();
+        try {
+            Connection con = ConnectionManager.getConnection();
+            String query = "SELECT "
+                    + "       *"
+                    + "   FROM"
+                    + "       BRANCH_OFFICE";
 
-    public static ArrayList<Product> getProduct() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            rs = ps.executeQuery(query);
+            if (rs != null) {
+                while (rs.next()) {
+                    Product product = new Product();
+                    product.setNameProduct(rs.getString("NAME"));
+                    product.setValues(rs.getDouble("VALUE"));
+                    product.setValuesSale(rs.getDouble("VALUESALE"));
+                    product.setAmount(rs.getInt("AMOUNT"));
+                    product.setDetails(rs.getString("DETAILS"));
+                    Product.add(product);
+                }
+            }
+
+            return Product;
+
+        } catch (Exception ex) {
+            throw ex;
+        }
+      
     }
 
-    public static boolean delete(int id) {
+    public static boolean delete() throws SQLException {
+
+      Connection con = ConnectionManager.getConnection();
+        String query = "DELETE "
+                + "         * "
+                + "     FROM "
+                + "         BRANCH_OFFICE"
+                + "     WHERE"
+                + "         ID=? ";
+        ps = con.prepareStatement(query);
+        ps.setInt(1, getId());
+        int updatedlines = ps.executeUpdate();
+        retorno = updatedlines > 0 ? true : false;
+        return retorno;
+    }
+    public static boolean update( int id,String nameProduct, double value, double valueSale, int amount, int stock,String details) throws Exception {
 
         boolean returnn = false;
 
-        /*  try {
-            Class.forName(DRIVER);
-            connection = DriverManager.getConnection(URL, LOGIN, SENHA);
-            PreparedStatement comand = connection.prepareStatement("DELETE FROM CLIENT WHERE ID_PRODUCT=?");
-                    
-            comand.setInt(1, id);
-            int lineserror = comand.executeUpdate();
-            if (lineserror > 0) {
-                returnn = true;
-            } else {
-                returnn = false;
-            }
-        } catch (SQLException ex) {
-            returnn = false;
-        } finally {
-            try {
-                connection.close();
-            } catch (SQLException ex) {
-                returnn = false;
-            }
+        try {
+            Connection con = ConnectionManager.getConnection();
+            String query = "UPDATE"
+                    + "       BRANCH_OFFICE"
+                    + "   SET"
+                    + "       NAME_PRODUCT='?',"
+                    + "       VALUE='?'"
+                    + "       VALUESALE='?'"
+                    + "       DETAILS='?',"
+                    + "       AMOUNT='?'"
+                    + "       STOKE='?'"
+                    + "   WHERE"
+                    + "       ID_PRODUCT=?";
+            ps = con.prepareStatement(query);
+
+            ps.setString(1, nameProduct);
+            ps.setDouble(2, value);
+            ps.setDouble(2, valueSale);
+            ps.setString(3, details);
+            ps.setInt(4, amount);
+            ps.setInt(5, stock);
+            ps.setInt(6,id);
+
+            int updatedlines = ps.executeUpdate();
+
+            retorno = updatedlines > 0 ? true : false;
+
+            return retorno;
+
+        } catch (Exception ex) {
+            throw ex;
         }
-        return returnn;
-    }*/
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public static boolean update(Product product) {
-
-        boolean returnn = false;
-
-        /*   try { 
-            
-            Class.forName(DRIVER); 
-            connection = DriverManager.getConnection(URL, LOGIN, SENHA); 
-            
-            PreparedStatement comand = connection.prepareStatement("UPDATE PRODUCT SET "
-                    + "NAME=?, AMOUNT=?, VALUES=?, VALUESSALE=?, DETAILS=?"); 
-            
-            comand.setString(1, product.getNameProduct());
-            comand.setInt(2, product.getAmount());
-            comand.setDouble(3, product.getValues());
-            comand.setDouble(4, product.getValeusSale());
-            comand.setString(5, product.getDetails());
-            
-            int lineserror = comand.executeUpdate(); 
-            
-             if (lineserror > 0) {
-                returnn = true;
-            } else {
-                returnn = false;
-            }
-        } catch (SQLException ex) {
-            returnn = false;
-        } finally { 
-            try { 
-                connection.close();
-            } catch (SQLException ex){ 
-                returnn = false; 
-            }
-        }
-        
-        return returnn; 
-        }  */
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public static boolean save(Product product) {
