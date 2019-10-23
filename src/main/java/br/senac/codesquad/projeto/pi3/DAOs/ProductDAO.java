6a660635e9d03fbcda306ba1aa69a0d9447e77bf
marcelo.moraes <marcelo.moraes1598@gmail.com>
2019-10-22 23:09:28 -0200

    Alteracao nas jsp de client

--- src/main/java/br/senac/codesquad/projeto/pi3/DAOs/ProductDAO.java
+++ src/main/java/br/senac/codesquad/projeto/pi3/DAOs/ProductDAO.java
@@ -171,4 +171,8 @@ public class ProductDAO {
         }
         return null;
     }
+
+    public static ArrayList<Product> read() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
