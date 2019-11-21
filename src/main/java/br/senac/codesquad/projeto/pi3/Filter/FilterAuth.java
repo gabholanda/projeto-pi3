c7a147af9f541475f85a374a760910f12e15504f
marcelopc <marcelopc@DESKTOP-9M7JUB1.Home>
2019-11-21 02:40:20 -0200

    teste auth/session

--- src/main/java/br/senac/codesquad/projeto/pi3/Filter/FilterAuth.java
+++ src/main/java/br/senac/codesquad/projeto/pi3/Filter/FilterAuth.java
@@ -3,97 +3,97 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.senac.codesquad.projeto.pi3.Filter;
-import br.senac.codesquad.projeto.pi3.enums.Roles;
-import br.senac.codesquad.projeto.pi3.models.User;
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+//package br.senac.codesquad.projeto.pi3.Filter;
+//import br.senac.codesquad.projeto.pi3.enums.Roles;
+//import br.senac.codesquad.projeto.pi3.models.User;
+//import java.io.IOException;
+//import javax.servlet.Filter;
+//import javax.servlet.FilterChain;
+//import javax.servlet.FilterConfig;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
 
 ///*
 // *
 // * @author marcelo.smoraes2
  
-@WebFilter(filterName = "FilterAuth", urlPatterns = {"/branch/", "/client/", "/product/", "/user/", "/sale/", "/report/",})
+//@WebFilter(filterName = "FilterAuth", urlPatterns = {"/branch/", "/client/", "/product/", "/user/", "/sale/", "/report/",})
 
-public class FilterAuth implements Filter {
+//public class FilterAuth implements Filter {
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+  //  @Override
+ //   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
+ //       HttpServletRequest httpRequest = (HttpServletRequest) request;
+ //       HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        HttpSession session = httpRequest.getSession();
+ //       HttpSession session = httpRequest.getSession();
 
-        if (session.getAttribute("usuario") == null) {
-            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
-            return;
-        }
+   //     if (session.getAttribute("usuario") == null) {
+       //     httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+     //       return;
+   //     }
 
-        User usuario = (User) session.getAttribute("usuario");
+//        User usuario = (User) session.getAttribute("usuario");
 
-        if (checkAuth(usuario, httpRequest)) {
+  //      if (checkAuth(usuario, httpRequest)) {
 
-            chain.doFilter(request, response);
-        } else {
+    //        chain.doFilter(request, response);
+ //       } else {
 
-            httpResponse.sendRedirect(httpRequest.getContextPath()
-                    + "/erro-nao-autorizado.jsp");
-        }
+   //         httpResponse.sendRedirect(httpRequest.getContextPath()
+     //               + "/erro-nao-autorizado.jsp");
+ //       }
 
-    }
+ //   }
 
-    private boolean checkAuth(User user, HttpServletRequest httpRequest) {
+ //   private boolean checkAuth(User user, HttpServletRequest httpRequest) {
 
-        String urlAcessada = httpRequest.getRequestURI();
+   //     String urlAcessada = httpRequest.getRequestURI();
 
-        if (urlAcessada.contains("/Home")) {
-            return true;
+     //   if (urlAcessada.contains("/Home")) {
+       //     return true;
 
-        } else if (urlAcessada.contains("/branch") && user.verificarPapel(Roles.DIRETORIA)) {
-            return true;
+//        } else if (urlAcessada.contains("/branch") && user.verificarPapel(Roles.DIRETORIA)) {
+ //           return true;
 
-        } else if (urlAcessada.contains("/report") && user.verificarPapel(Roles.DIRETORIA)) {
-            return true;
+ //       } else if (urlAcessada.contains("/report") && user.verificarPapel(Roles.DIRETORIA)) {
+   //         return true;
 
-        } else if (urlAcessada.contains("/branch") && user.verificarPapel(Roles.GERENTE)) {
-            return true;
-        } else if (urlAcessada.contains("/report") && user.verificarPapel(Roles.GERENTE)) {
-            return true;
+     //   } else if (urlAcessada.contains("/branch") && user.verificarPapel(Roles.GERENTE)) {
+   //         return true;
+  //      } else if (urlAcessada.contains("/report") && user.verificarPapel(Roles.GERENTE)) {
+    //        return true;
 
-        } else if (urlAcessada.contains("/product") && user.verificarPapel(Roles.BACKOFFICE)) {
-            return true;
+      //  } else if (urlAcessada.contains("/product") && user.verificarPapel(Roles.BACKOFFICE)) {
+ //           return true;
 
-        } else if (urlAcessada.contains("/user") && user.verificarPapel(Roles.RH)) {
-            return true;
+   //     } else if (urlAcessada.contains("/user") && user.verificarPapel(Roles.RH)) {
+     //       return true;
 
-        } else if (urlAcessada.contains("/user") && user.verificarPapel(Roles.TI)) {
-            return true;
+ //       } else if (urlAcessada.contains("/user") && user.verificarPapel(Roles.TI)) {
+   //         return true;
 
-        } else if (urlAcessada.contains("/sale") && user.verificarPapel(Roles.VENDAS)) {
-            return true;
-        } else if (urlAcessada.contains("/client") && user.verificarPapel(Roles.VENDAS)) {
-            return true;
-        }
-        return false;
-    }
+ //       } else if (urlAcessada.contains("/sale") && user.verificarPapel(Roles.VENDAS)) {
+   //         return true;
+  //      } else if (urlAcessada.contains("/client") && user.verificarPapel(Roles.VENDAS)) {
+    //        return true;
+  //      }
+    //    return false;
+ //   }
 
-    @Override
-    public void destroy() {
+ //   @Override
+ //   public void destroy() {
 
-    }
+  //  }
 
-    @Override
-    public void init(FilterConfig filterConfig) {
+ //   @Override
+ //   public void init(FilterConfig filterConfig) {
 
-    }
-}
+   // }
+//}
